5b289904b080ecbeb88dc66ecd3914f21390323b
Enzo Cazenave <encaze24@hotmail.com>
2023-09-27 08:25:09 -0300

    Add implementations of Stock and ListaDeStock

---
+++ tpo-enzo/src/impl/ListaDeStock.java
@@ -0,0 +1,27 @@
+package impl;
+
+import java.util.ArrayList;
+
+public class ListaDeStock {
+    private ArrayList<Stock> listaDeStock;
+
+    ListaDeStock() {
+        this.listaDeStock = new ArrayList<Stock>();
+    }
+
+    public Stock getStockById(int codigoProducto) {
+        for (Stock S: listaDeStock) {
+            if (S.codigoProducto == codigoProducto) return S;
+        }
+    }
+
+    public void addProductStock(int codigoProducto, int stock, int stockMinimo) {
+        Stock productStock = new Stock(codigoProducto, stock, stockMinimo);
+        listaDeStock.add(productStock);
+    }
+
+    public void decrementStockById(int codigoProducto, int stock) {
+        Stock productStock = this.getStockById(codigoProducto);
+        productStock.decrementStock(stock);
+    }
+}
---
+++ tpo-enzo/src/impl/Stock.java
@@ -0,0 +1,19 @@
+package impl;
+
+public class Stock {
+    int codigoProducto;
+    int stock;
+    int stockMinimo;
+
+    Stock(int codigoProducto, int stock, int stockMinimo) {
+        this.codigoProducto = codigoProducto;
+        this.stock = stock;
+        this.stockMinimo = stockMinimo;
+    }
+
+    public void decrementStock(int stock) {
+        int decrementQtty = this.stock - stock;
+        if (decrementQtty <= 0) return;
+        this.stock = decrementQtty;
+    }
+}
